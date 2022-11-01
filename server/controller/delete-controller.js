92cf2c91efefd597307b59902a8107c322b12c14
Lucian <ciucla.lucian@gmail.com>
2022-11-01 15:39:17 +0200

    Delete mission implementation

--- server/controller/delete-controller.js
+++ server/controller/delete-controller.js
@@ -1,19 +1,19 @@
-const {  } = require("../../database/elastic");
+const { deleteKeyElastic } = require("../../database/elastic");
 const { ES, errorLogFile, logFile } = require('../../conf.json');
 const { insertLog } = require('../../Logs/formatLogs');
 
 const deleteMission = async (req, res, next) => {
     const mission = req.params.mission;
-    if (!mission) {
-        throw {
-            error: true,
-            msg: 'Mission param cant be null!',
-            errorStatus: 4
-        }
-    }
     try {
-
-        res.json({});
+        if (!mission) {
+            throw {
+                error: true,
+                msg: 'Mission param cant be null!',
+                errorStatus: 4
+            }
+        }
+        let responseDelete = await deleteKeyElastic("mission.keyword", mission, ES.INDEX_MISIUNI);
+        res.json(responseDelete);
     } catch (error) {
         insertLog(error, errorLogFile);
         res.json({
